f111f74f3b027a61b4631b1cd92d3d208f9dd077
Joe Gorman <jgorman3@lion.lmu.edu>
2018-07-05 20:15:18 -0400

    Create Fourier.py
    writing out initial methods

---
+++ Fourier.py
@@ -0,0 +1,13 @@
+import math
+
+#Fourier Class will hold efficient methods to solve more complicated communications problems later
+class Fourier():
+
+  def fourierseries():
+  
+  def fouriertransform():
+  
+  def dfourierseries():
+  
+  def dfouriertransform():
+
